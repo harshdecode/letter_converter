56fc5708e480f562717a0ed13da5c526e211a50f
harshdecode <harshs6991@gmail.com>
2023-07-31 14:38:48 +0530

    helper

--- app/helpers/letter_converter/convert_helper.rb
+++ app/helpers/letter_converter/convert_helper.rb
@@ -1,7 +1,7 @@
 module LetterConverter
   module ConvertHelper
     def to_uppercase(input_string)
-      output_string = input_string.to_s.upcase
+      output_string = input_string.upcase
       { input: input_string, output: output_string }
     end
   end

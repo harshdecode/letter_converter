ba2a78ef327d14e67ff844fd1fbbc79371f17f1c
harshdecode <harshs6991@gmail.com>
2023-07-31 12:07:30 +0530

    controller to helper

--- app/helpers/letter_converter/convert_helper.rb
+++ app/helpers/letter_converter/convert_helper.rb
@@ -1,4 +1,9 @@
 module LetterConverter
   module ConvertHelper
+  def to_uppercase
+        input_string = params[:input_string]
+        output_string = LetterConverter.to_uppercase(input_string)
+        render json: { input: input_string, output: output_string }
+      end
   end
 end
